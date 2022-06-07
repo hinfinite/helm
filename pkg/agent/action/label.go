7c0df48fba25d29133700487c68f3cba6000f5cb
11010 <jian.zhang02@hand-china.com>
2022-06-07 14:34:49 +0800

    refactor: 安装chart时任何chart中的资源都会打上基础标签

--- pkg/agent/action/label.go
+++ pkg/agent/action/label.go
@@ -179,8 +179,7 @@ func AddLabel(imagePullSecret []v1.LocalObjectReference,
 	case "Pod":
 		addAppLabels()
 	default:
-		glog.Warningf("Skipping to add choerodon label, object: Kind %s of Release %s", kind, releaseName)
-		return nil
+		addAppLabels()
 	}
 	if t.GetNamespace() != "" && t.GetNamespace() != namespace {
 		return fmt.Errorf(" Kind:%s Name:%s. The namespace of this resource is not consistent with helm release", kind, t.GetName())
